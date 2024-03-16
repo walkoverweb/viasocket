181e3358a5a6977a320129d8b29885bf72fba4e3
piyusssshh <piyush@whozzat.com>
2024-03-16 15:45:26 +0530

    PIYUSSSSHH | CREATE | featureGrid in indec

--- src/components/productComp/productComp.js
+++ src/components/productComp/productComp.js
@@ -1,19 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./productComp.module.scss";
 import TrustedBy from "../trustedBy/trustedBy";
 import TemplateSection from "../templateSection/templateSection";
 import GetStarted from "../getStarted/getStarted";
-import { HiOutlineComputerDesktop } from "react-icons/hi2";
+import { FeaturesGrid } from "../featureGrid/featureGrid";
 
 export default function ProductComp(props) {
   let pageData = props?.productData.find(
     (page) => page?.name?.toLowerCase() === props?.page
   );
-  // const parseBlockType = (blockType) => {
-  //   const [rowSpan, colSpan] = blockType.match(/\d+/g).map(Number);
-  //   return { rowSpan, colSpan };
-  // };
+
 
   return (
     <>
@@ -53,158 +49,7 @@ export default function ProductComp(props) {
 
           <TrustedBy data={props?.trustedBy} />
         </div>
-        <div className='grid gap-10 container'>
-          <h2 className='text-3xl font-semibold'>Features</h2>
-
-          <div className='grid xl:grid-cols-4 lg:grid-cols-3 lg:grid-rows-3 md:grid-cols-3 md:grid-rows-4 sm:grid-cols-2 sm:grid-rows-6 md:gap-5 gap-3  overflow-hidden '>
-            {props?.features
-              .sort((a, b) => a.priority - b.priority)
-              .map((feature, index) => {
-                if (feature?.product?.toLowerCase() === props?.page) {
-                  if (feature.block_type === "R2C2") {
-                    return (
-                      <Link
-                        key={index}
-                        href={feature?.link ? feature?.link : "#"}
-                        className={`${styles.r2c2} col-span-2 row-span-2  bg-[#F7F7F8] rounded-lg justify-between gap-4 w-full h-full flex flex-col aspect-square overflow-hidden `}
-                      >
-                        <div className='flex flex-col gap-3 lg:p-5 md:p-3 p-3'>
-                          {feature?.icon ? (
-                            <feature.icon size={35} />
-                          ) : (
-                            <HiOutlineComputerDesktop size={35} />
-                          )}
-                          <div className='flex flex-col gap-2 justify-end'>
-                            <h1 className='md:text-xl text-lg font-semibold'>
-                              {feature.name}
-                            </h1>
-                            <p className='md:text-sm text-xs text-gray-500'>
-                              {feature?.description}
-                            </p>
-                            {/* {feature?.link && (
-                              <Link href={feature?.link}>
-                                <button className='underline md:text-sm text-xs text-blue-500'>
-                                  {" "}
-                                  Discover{" "}
-                                </button>
-                              </Link>
-                            )} */}
-                          </div>
-                        </div>
-                        {/* <Image
-                              src="https://placehold.co/900x1600"
-                              alt="Placeholder"
-                              className={`${styles.r2c2_img} `}
-                              height={1080}
-                              width={1080}
-                            /> */}
-                        <div
-                          className={`${styles.r2c2_img_cont} flex h-full justify-end items-end`}
-                        >
-                          {feature?.image ? (
-                            <Image
-                              src={feature.image}
-                              alt='feature 1'
-                              className={`${styles.r2c2_img} `}
-                              height={1080}
-                              width={1080}
-                            />
-                          ) : (
-                            <Image
-                              src='https://placehold.co/1600x1400'
-                              alt='Placeholder'
-                              className={`${styles.r2c2_img} `}
-                              height={1080}
-                              width={1080}
-                            />
-                          )}
-                        </div>
-                      </Link>
-                    );
-                  } else if (feature.block_type === "R1C2") {
-                    return (
-                      <Link
-                        key={index}
-                        href={feature?.link ? feature?.link : "#"}
-                        className={`${styles.r1c2} bg-[#F7F7F8]  col-span-2 row-span-1 flex sm:flex-row flex-col rounded-lg w-full h-full`}
-                      >
-                        <div className='flex flex-col gap-3 h-full justify-between  lg:p-5 md:p-3 p-3 sm:w-1/2 w-1/1'>
-                          {feature?.icon ? (
-                            <feature.icon size={35} />
-                          ) : (
-                            <HiOutlineComputerDesktop size={35} />
-                          )}
-                          <div className='flex flex-col gap-2 justify-end'>
-                            <h1 className='md:text-xl text-lg font-semibold'>
-                              {feature.name}
-                            </h1>
-                            <p className='md:text-sm text-xs text-gray-500'>
-                              {feature?.description}
-                            </p>
-                          </div>
-                        </div>
-
-                        <div
-                          className={`${styles.r1c2_img_cont} flex justify-end items-end sm:w-1/2 w-1/1 h-auto`}
-                        >
-                          {feature?.image ? (
-                            <Image
-                              src={feature.image}
-                              alt='feature 1'
-                              className={`${styles.r1c2_img} `}
-                              height={1080}
-                              width={1080}
-                            />
-                          ) : (
-                            <Image
-                              src='https://placehold.co/1600x1600'
-                              alt='Placeholder'
-                              className={`${styles.r1c2_img} `}
-                              height={1080}
-                              width={1080}
-                            />
-                          )}
-                        </div>
-                      </Link>
-                    );
-                  } else {
-                    return (
-                      <Link
-                        href={feature?.link ? feature?.link : "#"}
-                        key={index}
-                        className={`col-span-2 md:col-span-1 row-span-1 min-h-[200px] bg-[#F7F7F8] lg:p-5 md:p-3 p-3 rounded-lg justify-between gap-1  flex flex-col  md:aspect-square w-full h-full`}
-                      >
-                        <div className='flex flex-col gap-3'>
-                          {feature?.icon ? (
-                            <feature.icon size={35} />
-                          ) : (
-                            <HiOutlineComputerDesktop size={35} />
-                          )}
-                        </div>
-
-                        <div className='flex flex-col gap-2 justify-end '>
-                          <h1 className='md:text-xl text-lg  font-semibold'>
-                            {feature.name}
-                          </h1>
-                          <p className='md:text-sm text-xs text-gray-500'>
-                            {feature?.description}
-                          </p>
-                          {/* {feature?.link && (
-                            <Link href={feature?.link}>
-                              <button className='underline md:text-sm text-xs text-blue-500'>
-                                {" "}
-                                Discover{" "}
-                              </button>
-                            </Link>
-                          )} */}
-                        </div>
-                      </Link>
-                    );
-                  }
-                }
-              })}
-          </div>
-        </div>
+       {props?.features && <FeaturesGrid features={props?.features} page={props?.page}/>}
         {props?.templatedata && <TemplateSection data={templates} />}
         {props?.tablevsspreadsheet && (
           <div className='grid gap-10 container'>
