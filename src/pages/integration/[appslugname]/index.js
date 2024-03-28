c27a8ab1f9f7f2f08f103916c00d368864ef9352
piyusssshh <piyush@whozzat.com>
2024-03-28 16:43:41 +0530

    PIYUSSSSHH | UPDATE | 404 condition update

--- src/pages/integration/[appslugname]/index.js
+++ src/pages/integration/[appslugname]/index.js
@@ -571,7 +571,7 @@ const IntegrationSlugPage = ({ combos, apps, pathArray }) => {
     } else {
         return (
             <>
-                <ErrorComp />
+                <ErrorComp pathArray={pathArray} />
             </>
         )
     }
