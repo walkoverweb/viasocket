3028eb461de8def30fd38f5702f05cecdaaea374
piyusssshh <piyush@whozzat.com>
2024-04-04 15:45:01 +0530

    PIYUSSSSHH | UPDATE | noindex env updated

--- src/components/headComp/headComp.js
+++ src/components/headComp/headComp.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 
 export default function HeadComp() {
-    if (process.env.NEXT_PUBLIC_PRODUCTION_ENVIRONMENT !== 'production') {
+    if (
+        process.env.NEXT_PUBLIC_PRODUCTION_ENVIRONMENT !== 'production' &&
+        !process.env.NEXT_PUBLIC_PRODUCTION_ENVIRONMENT
+    ) {
         return (
             <>
                 <Head>
