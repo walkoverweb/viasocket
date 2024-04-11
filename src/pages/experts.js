a7ebb707071caf63dffd3ac105f76e9e6b1c8115
piyusssshh <piyush@whozzat.com>
2024-04-11 12:54:58 +0530

    PIYUSSSSHH | UPDATE | experts page design updated

--- src/pages/experts.js
+++ src/pages/experts.js
@@ -1,7 +1,9 @@
 import { getDbdashData } from './api';
 import AgencyList from '@/components/agencyList/agnecyList';
 import MetaHeadComp from '@/components/metaHeadComp/metaHeadComp';
-import { MdArticle } from 'react-icons/md';
+import { MdArticle, MdChevronRight, MdOutlineArticle } from 'react-icons/md';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export async function getServerSideProps() {
     const IDs = ['tblajmg8e', 'tblmsw3ci', 'tbl2bk656', 'tblirrj24'];
@@ -20,7 +22,6 @@ export async function getServerSideProps() {
 }
 
 const Experts = ({ agencies, rawPageData, pathArray, metaData, expertsHelp }) => {
-    console.log('🚀 ~ Experts ~ expertsHelp:', expertsHelp);
     let pageData = rawPageData.find((page) => page?.name?.toLowerCase() === pathArray[1]);
 
     let verifiedArr = [];
@@ -44,63 +45,76 @@ const Experts = ({ agencies, rawPageData, pathArray, metaData, expertsHelp }) =>
         <>
             <MetaHeadComp metaData={metaData} page={'/experts'} pathArray={pathArray} />
 
-            <div id="iframe-parent-container" class="popup-parent-container">
-                <div className="header">
-                    <h4 id="title">Title</h4>
-                </div>
-                <iframe id="iframe-component" title="iframe"></iframe>
-            </div>
-            {/* </div> */}
-
-            <button id="interfaceEmbed" class="popup-interfaceEmbed" aria-label="interface">
-                Interface
-            </button>
-            <div className="container">
-                <div className=" grid mt-14 gap-8 w-full  md:pt-24 pt-24 ">
+            <div className="">
+                <div className="grid gap-9 md:w-5/6 w=1/1 py-24 container">
                     {pageData?.h3 && <h3 className="text-2xl">{pageData?.h3}</h3>}
-                    <div className="grid gap-2 md:w-5/6 w=1/1">
-                        {pageData?.h1 && <h1 className="md:text-6xl text-4xl font-medium ">{pageData?.h1}</h1>}
-                        {pageData?.h2 && <h3 className="text-2xl">{pageData?.h2}</h3>}
-                    </div>
+                    {pageData?.h1 && <h1 className="md:text-6xl text-4xl font-medium ">{pageData?.h1}</h1>}
+                    {pageData?.h2 && <h3 className="text-2xl">{pageData?.h2}</h3>}
                 </div>
 
-                <div className="grid gap-8">
-                    <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold mt-20 text-[#415765]">
-                        Verified automation agencies by viaSocket
-                    </h2>
+                <div className="flex flex-col gap-9 py-24 container">
+                    <h2 className="text-3xl font-semibold ">Verified automation agencies by viaSocket</h2>
                     {verifiedArr.length > 0 && <AgencyList agencies={verifiedArr} type={'verified'} />}
                 </div>
-                <div className="grid gap-8">
-                    <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold mt-20 text-[#415765]">
-                        Non-verified automation agencies
-                    </h2>
+                <div className="flex flex-col gap-9 py-24 container">
+                    <h2 className="text-3xl font-semibold ">Non-verified automation agencies</h2>
                     {nonVerifiedArr.length > 0 && <AgencyList agencies={nonVerifiedArr} type={'nonverified'} />}
-                    <div className="mt-10 text-center">
-                        <button
-                            className="px-3.5 sm:px-6 py-4 font-bold text-xs sm:text-lg md:text-xl  lg:text-2xl border border-black text-black rounded"
-                            aria-label="Suggest an agency or get listed if you are one"
-                        >
-                            Suggest an agency or get listed if you are one
-                        </button>
+                </div>
+                <div className="py-24  bg-white">
+                    <div className="container flex flex-col gap-9 ">
+                        <h2 className="text-3xl font-semibold ">
+                            Register your agency as our verified <br />
+                            Experts for automations.
+                        </h2>
+                        <div className="flex justify-between items-center">
+                            <div className="flex flex-col gap-9 w-1/2 h-full justify-center">
+                                <p>
+                                    As a viaSocket expert, you'll empower clients with tailor-made automation solutions,
+                                    training, and unwavering support every step of the way. 
+                                    <br />
+                                    <br />
+                                    With viaSocket in your arsenal, you can design, implement, and maintain innovative
+                                    solutions, ensuring you and your clients stay ahead of the curve. 
+                                    <br />
+                                    <br />
+                                    Let your agency's knowledge and skills set the foundation for countless success
+                                    stories.
+                                </p>
+                                <Link target="_blank" href="https://calendly.com/rpaliwal71/15-mins?month=2024-03">
+                                    <button className="btn btn-md btn-accent rounded-sm w-fit">Be an Expert</button>
+                                </Link>
+                            </div>
+                            <Image
+                                src={'https://placehold.co/600x600'}
+                                className="w-auto max-w-1/2 h-full max-h-[500px]"
+                                width={1080}
+                                height={1080}
+                                alt="be an expert image"
+                            />
+                        </div>
                     </div>
                 </div>
-                <h2
-                    className="text-base sm:text-xl lg:text-2xl mt-6 block underline text-[#415765]"
-                    rel="stylesheet"
-                    href="#"
-                >
-                    How experts can help?
-                </h2>
-                <div className="mt-6 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-center md:justify-start gap-6">
-                    {expertsHelp &&
-                        expertsHelp.map((expertsHelpBlog, index) => {
-                            return (
-                                <div>
-                                    <MdArticle color="#8F9396" fontSize={30} />
-                                    <p>{expertsHelpBlog?.description}</p>
-                                </div>
-                            );
-                        })}
+                <div className="flex flex-col gap-9 my-24 container">
+                    <h2 className="text-3xl font-semibold">How experts can help?</h2>
+                    <div className="mt-6 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-center md:justify-start gap-6">
+                        {expertsHelp &&
+                            expertsHelp.map((expertsHelpBlog, index) => {
+                                return (
+                                    <div key={index} className="flex flex-col gap-4 bg-white p-6 rounded-md">
+                                        <MdOutlineArticle color="#8F9396" fontSize={36} />
+                                        <p className="text-xl ">{expertsHelpBlog?.description}</p>
+                                        {expertsHelpBlog?.link && (
+                                            <Link
+                                                href={expertsHelpBlog?.link}
+                                                className="flex items-center mt-auto text-[#4485F2]"
+                                            >
+                                                Learn More <MdChevronRight fontSize={22} />
+                                            </Link>
+                                        )}
+                                    </div>
+                                );
+                            })}
+                    </div>
                 </div>
             </div>
         </>
