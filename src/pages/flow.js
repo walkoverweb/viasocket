d6a1f8d2fe766230927ebfc1ec0f00ca0d3e536e
piyusssshh <piyush@whozzat.com>
2024-04-12 19:03:42 +0530

    PIYUSSSSHH | CREATE | new flow page desig created

--- src/pages/flow.js
+++ src/pages/flow.js
@@ -4,11 +4,25 @@ import GetStarted from '@/components/getStarted/getStarted';
 import MetaHeadComp from '@/components/metaHeadComp/metaHeadComp';
 import Image from 'next/image';
 import { MdClose } from 'react-icons/md';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+import { useEffect, useState } from 'react';
+import { FeaturesGrid } from '@/components/featureGrid/featureGrid';
+
 export async function getServerSideProps() {
     const IDs = ['tblsaw4zp', 'tblvgm05y', 'tblmsw3ci', 'tblvo36my', 'tbl2bk656'];
 
     const dataPromises = IDs.map((id) => getDbdashData(id));
     const results = await Promise.all(dataPromises);
+
+    const apiHeaders = {
+        headers: {
+            'auth-key': process.env.NEXT_PUBLIC_INTEGRATION_KEY,
+        },
+    };
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_INTEGRATION_URL}/all?limit=200`, apiHeaders);
+    const apps = await response.json();
+    console.log('🚀 ~ getServerSideProps ~ apps:', apps);
 
     return {
         props: {
@@ -17,47 +31,76 @@ export async function getServerSideProps() {
             productData: results[2].data.rows,
             features: results[3].data.rows,
             metaData: results[4].data.rows,
+            apps,
         },
     };
 }
 
-const Flow = ({ trustedBy, getStartedData, productData, features, pathArray, metaData }) => {
+const Flow = ({ trustedBy, getStartedData, productData, features, pathArray, metaData, apps }) => {
+    const [slectedApps, setSelectedApps] = useState([]);
+    const handleOnSelect = (item) => {
+        setSelectedApps((prevSelectedApps) => [...prevSelectedApps, item]);
+    };
+
+    const handleGeneration = () => {
+        console.log('hello');
+    };
+    const removeAppFromArray = (indexToRemove) => {
+        if (indexToRemove >= 0 && indexToRemove < slectedApps.length) {
+            const newSelectedApps = slectedApps.filter((_, index) => index !== indexToRemove);
+            setSelectedApps(newSelectedApps);
+        }
+    };
+
+    useEffect(() => {
+        if (apps.length > 0) {
+            setSelectedApps(apps.slice(0, 5));
+        }
+    }, [apps]);
     return (
         <>
             <MetaHeadComp metaData={metaData} page={'/flow'} pathArray={pathArray} />
             <div>
-                <div className="py-container container">
-                    <h1 className="text-6xl">Let your apps talk</h1>
-                    <h2 className="text-2xl">Automate mundane tasks with AI in few clicks</h2>
-                </div>
-                <div className="py-container container flex flex-col gap-9">
-                    <h2 className="text-2xl font-bold">Explore topautomations for the software you use</h2>
-                    <div className="flex flex-wrap gap-4">
-                        {Array.from({ length: 19 }, (_, index) => (
-                            <div key={index} className="flex items-center gap-2 bg-white w-fit p-3 rounded ">
-                                <Image src={'https://placehold.co/40x40'} width={24} height={24} alt="ico" />
-                                <span>Airtabe</span>
-                                <MdClose />
-                            </div>
-                        ))}
+                <div className="py-container container flex flex-col gap-14">
+                    <div className="gap-4 flex flex-col md:w-2/3">
+                        <h1 className="md:text-6xl text-4xl font-medium ">Let your apps talk</h1>
+                        <h2 className="text-2xl">Automate mundane tasks with AI in few clicks</h2>
                     </div>
-                    <label className="input input-bordered flex items-center gap-2 w-1/3 h-auto p-2 bg-white">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            fill="currentColor"
-                            className="w-4 h-4 opacity-70"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                        <input type="text" className="grow" placeholder="Search" />
-                    </label>
+
+                    <div className=" flex flex-col gap-6">
+                        <h2 className="text-2xl font-bold">Explore topautomations for the software you use</h2>
+                        <div className="flex flex-wrap gap-4">
+                            {slectedApps.map((app, index) => {
+                                return (
+                                    <div
+                                        key={index}
+                                        className="flex items-center gap-2 bg-white w-fit p-3 rounded cursor-pointer "
+                                    >
+                                        <Image src={app?.iconurl} width={24} height={24} alt="ico" />
+                                        <span>{app?.name}</span>
+                                        <MdClose
+                                            className="text-gray-300 hover:text-gray-950"
+                                            onClick={() => {
+                                                removeAppFromArray(index);
+                                            }}
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        <ReactSearchAutocomplete
+                            items={apps}
+                            className="lg:w-1/3 sm:w-2/3 w-full"
+                            onSelect={handleOnSelect}
+                            autoFocus
+                        />
+                        <button className="btn btn-primary w-fit btn-md rounded" onClick={handleGeneration}>
+                            Generate recommendations by AI
+                        </button>
+                    </div>
                 </div>
             </div>
+            {features && <FeaturesGrid features={features} page={pathArray[1]} />}
             <div className="container my-24">
                 {getStartedData && <GetStarted data={getStartedData} isHero={'false'} />}
             </div>
