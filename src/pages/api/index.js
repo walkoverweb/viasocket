2b0a81c6374cfb621e44edd4cd7fcd7c967437c8
piyusssshh <piyush@whozzat.com>
2024-04-17 14:14:32 +0530

    PIYUSSSSHH | HOTFIX | DBdash base url change and uplpaded in env

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,31 +1,31 @@
 //fetch data from DBDash
 export const getDbdashData = async (tableName) => {
-    const apiUrl = `https://dbdash-backend-h7duexlbuq-el.a.run.app/65d2ed33fa9d1a94a5224235/${tableName}`
-    const authKey = 'keyJZz-15yE8MAp' // Replace 'AUTH_TOKEN' with your actual authentication token
+    const apiUrl = `${process.env.NEXT_PUBLIC_DB_BASE_URL}/65d2ed33fa9d1a94a5224235/${tableName}`;
+    const authKey = ''; // Replace 'AUTH_TOKEN' with your actual authentication token
 
     try {
         const response = await fetch(apiUrl, {
             headers: {
-                'auth-key': `keyJZz-15yE8MAp`,
+                'auth-key': `${process.env.NEXT_PUBLIC_DB_KEY}`,
             },
-        })
-        const responseData = await response.json()
-        return responseData
+        });
+        const responseData = await response.json();
+        return responseData;
     } catch (error) {
-        console.error('Error:', error)
+        console.error('Error:', error);
     }
-}
+};
 
 //fetch data from multiple tables in DB Dash
 export default async function fetchDataAndUpdateState(IDs, getDataFunction) {
     for (let i = 0; i < IDs.length; i++) {
-        const ID = IDs[i]
-        const dbdashData = await getDataFunction(ID)
+        const ID = IDs[i];
+        const dbdashData = await getDataFunction(ID);
     }
 }
 
 export const integration = async () => {
-    const apiUrl = `https://dbdash-backend-h7duexlbuq-el.a.run.app/64f58cfe54919de3f250dc6d/tblwegm8v?limit=25%27`
+    const apiUrl = `https://dbdash-backend-h7duexlbuq-el.a.run.app/64f58cfe54919de3f250dc6d/tblwegm8v?limit=25%27`;
 
     // const authKey = 'keyc9x2Q54-PHlP';
     // try{
@@ -42,4 +42,4 @@ export const integration = async () => {
     //   return responseData;
     // }  catch (error) {
     // }
-}
+};
