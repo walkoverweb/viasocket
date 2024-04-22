cb661cf59c2d5de51cb68ead4d50a52623a4af05
piyusssshh <piyush@whozzat.com>
2024-04-22 14:35:54 +0530

    PIYUSSSSHH ||

--- src/components/headComp/headComp.js
+++ src/components/headComp/headComp.js
@@ -1,20 +1,20 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function HeadComp() {
     return (
         <>
             <Head>
+                <meta
+                    http-equiv="Content-Security-Policy"
+                    content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://cdnjs.cloudflare.com "
+                />
+
                 {!process.env.NEXT_PUBLIC_PRODUCTION_ENVIRONMENT ||
-                    (process.env.NEXT_PUBLIC_PRODUCTION_ENVIRONMENT !==
-                        'production' && (
+                    (process.env.NEXT_PUBLIC_PRODUCTION_ENVIRONMENT !== 'production' && (
                         <meta name="robots" content="noindex, nofollow" />
                     ))}
-                <link
-                    rel="icon"
-                    type="image/x-icon"
-                    href="/assets/brand/fav_ico.svg"
-                />
+                <link rel="icon" type="image/x-icon" href="/assets/brand/fav_ico.svg" />
             </Head>
         </>
-    )
+    );
 }
