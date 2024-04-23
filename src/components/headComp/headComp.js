2225909d6ab6450b0a591919f24fc6e1804dfd41
Piyush Sawaye _ <83305138+piyusssshh@users.noreply.github.com>
2024-04-23 10:46:11 +0530

    Update headComp.js

--- src/components/headComp/headComp.js
+++ src/components/headComp/headComp.js
@@ -4,10 +4,7 @@ export default function HeadComp() {
     return (
         <>
             <Head>
-                <meta
-                    http-equiv="Content-Security-Policy"
-                    content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://cdnjs.cloudflare.com "
-                />
+          
 
                 {!process.env.NEXT_PUBLIC_PRODUCTION_ENVIRONMENT ||
                     (process.env.NEXT_PUBLIC_PRODUCTION_ENVIRONMENT !== 'production' && (
