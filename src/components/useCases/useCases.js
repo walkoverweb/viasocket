1355113902d47d6cc25ebff44ee0a945101e4704
Vanshika58 <guptavanshika9630@gmail.com>
2024-07-04 12:26:06 +0530

    VANSHIKA || CSS AND DESIGN COMPLETED

--- src/components/useCases/useCases.js
+++ src/components/useCases/useCases.js
@@ -1,3 +1,79 @@
+// import React, { useState } from 'react';
+
+// export default function UseCase() {
+//     const usecases = [
+//         {
+//             'heading': 'Automate CI/CD workflows',
+//             'content': [
+//                 'Integrate Docker with GitHub to automatically build and deploy Docker containers when new code is pushed to a GitHub repository',
+//                 'Integrate Docker with Bitbucket to trigger Docker container builds and deployments whenever new commits are made in Bitbucket.',
+//             ],
+//         },
+//         {
+//             'heading': 'Set up automated testing environments',
+//             'content': [
+//                 'Integrate Docker with Jenkins to automatically set up Docker-based testing environments for continuous integration processes.',
+//                 'Integrate Docker with CircleCI to create and manage Docker containers for automated testing environments as part of the CI workflow.',
+//             ],
+//         },
+//         {
+//             'heading': 'Process data automatically',
+//             'content': [
+//                 'Integrate Docker with Google Sheets to deploy Docker containers that process and update data entries in Google Sheets',
+//                 'Integrate Docker with Airtable to launch Docker containers for processing records and automating workflows in Airtable',
+//             ],
+//         },
+//         {
+//             'heading': 'Scale web applications dynamically',
+//             'content': [
+//                 'Integrate Docker with Azure Functions to dynamically manage and scale Docker containers based on Azure Functions events',
+//                 'Integrate Docker with AWS Lambda to automatically scale Dockerized web applications using AWS Lambda triggers.',
+//             ],
+//         },
+//     ];
+
+//     const [selectedUsecase, setSelectedUsecase] = useState(0);
+
+//     const handleUsecaseClick = (index) => {
+//         setSelectedUsecase(index);
+//     };
+
+//     return (
+//         <div className="container mx-auto px-4 py-8">
+//             <div className="flex flex-col md:flex-row">
+//                 <div className="p-4 rounded-md w-full md:w-1/2">
+//                     <h1 className="text-5xl font-bold mb-4">Use Cases</h1>
+//                     <ul className="list-disc pl-4">
+//                         {usecases.map((usecase, index) => (
+//                             <div key={index} className="border-b-4 border-black-400 border-opacity-100 border-color:black;">
+//                                 <li
+//                                     className={`cursor-pointer flex items-center px-4 py-6 ${selectedUsecase === index ? 'bg-gray-200' : ''}`}
+//                                     onClick={() => handleUsecaseClick(index)}
+//                                 >
+//                                     {usecase.heading}
+//                                     {selectedUsecase === index && (
+//                                         <span className="ml-2 w-3 h-3 rounded-full bg-sky-500 "></span>
+//                                     )}
+//                                 </li>
+//                             </div>
+//                         ))}
+//                     </ul>
+//                 </div>
+//                 {selectedUsecase !== null && (
+//                     <div className="bg-gray p-4 rounded-md w-full md:w-1/2 ml-5 mx-auto content-center">
+//                         <h2 className="text-4xl font-bold mb-2">{usecases[selectedUsecase].heading}</h2>
+//                         <ul className="list-disc pl-4">
+//                             {usecases[selectedUsecase].content.map((description, index) => (
+//                                 <li key={index}>{description}</li>
+//                             ))}
+//                         </ul>
+//                     </div>
+//                 )}
+//             </div>
+//         </div>
+//     );
+// }
+
 import React, { useState } from 'react';
 
 export default function UseCase() {
@@ -5,43 +81,43 @@ export default function UseCase() {
         {
             'heading': 'Automate CI/CD workflows',
             'content': [
-                'Integrate Docker with GitHub  to automatically build and deploy Docker containers when new code is pushed to a GitHub repository',
-                'Integrate Docker with Bitbucket  to trigger Docker container builds and deployments whenever new commits are made in Bitbucket.',
+                'Integrate Docker with GitHub to automatically build and deploy Docker containers when new code is pushed to a GitHub repository',
+                'Integrate Docker with Bitbucket to trigger Docker container builds and deployments whenever new commits are made in Bitbucket.',
             ],
         },
         {
             'heading': 'Set up automated testing environments',
             'content': [
-                'Integrate Docker with Jenkins  to automatically set up Docker-based testing environments for continuous integration processes.',
-                'Integrate Docker with CircleCI  to create and manage Docker containers for automated testing environments as part of the CI workflow.',
+                'Integrate Docker with Jenkins to automatically set up Docker-based testing environments for continuous integration processes.',
+                'Integrate Docker with CircleCI to create and manage Docker containers for automated testing environments as part of the CI workflow.',
             ],
         },
         {
             'heading': 'Process data automatically',
             'content': [
-                'Integrate Docker with Google Sheets  to deploy Docker containers that process and update data entries in Google Sheets',
-                'Integrate Docker with Airtable  to launch Docker containers for processing records and automating workflows in Airtable',
+                'Integrate Docker with Google Sheets to deploy Docker containers that process and update data entries in Google Sheets',
+                'Integrate Docker with Airtable to launch Docker containers for processing records and automating workflows in Airtable',
             ],
         },
         {
             'heading': 'Scale web applications dynamically',
             'content': [
-                'Integrate Docker with Azure Functions  to dynamically manage and scale Docker containers based on Azure Functions events',
-                'Integrate Docker with AWS Lambda  to automatically scale Dockerized web applications using AWS Lambda triggers.',
+                'Integrate Docker with Azure Functions to dynamically manage and scale Docker containers based on Azure Functions events',
+                'Integrate Docker with AWS Lambda to automatically scale Dockerized web applications using AWS Lambda triggers.',
+            ],
+        },
+        {
+            'heading': 'Process data automatically',
+            'content': [
+                'Integrate Docker with Google Sheets to deploy Docker containers that process and update data entries in Google Sheets',
+                'Integrate Docker with Airtable to launch Docker containers for processing records and automating workflows in Airtable',
             ],
         },
         {
             'heading': 'Scale web applications dynamically',
             'content': [
-                'Integrate Docker with Azure Functions  to dynamically manage and scale Docker containers based on Azure Functions events',
-                'Integrate Docker with AWS Lambda  to automatically scale Dockerized web applications using AWS Lambda triggers.',
-            ],
-        },
-        {
-            'heading': 'Scale web applications dynamically',
-            'content': [
-                'Integrate Docker with Azure Functions  to dynamically manage and scale Docker containers based on Azure Functions events',
-                'Integrate Docker with AWS Lambda  to automatically scale Dockerized web applications using AWS Lambda triggers.',
+                'Integrate Docker with Azure Functions to dynamically manage and scale Docker containers based on Azure Functions events',
+                'Integrate Docker with AWS Lambda to automatically scale Dockerized web applications using AWS Lambda triggers.',
             ],
         },
     ];
@@ -51,23 +127,27 @@ export default function UseCase() {
     const handleUsecaseClick = (index) => {
         setSelectedUsecase(index);
     };
+
     return (
         <div className="container mx-auto px-4 py-8">
             <div className="flex flex-col md:flex-row">
-                {' '}
                 <div className="p-4 rounded-md w-full md:w-1/2">
-                    {' '}
                     <h1 className="text-5xl font-bold mb-4">Use Cases</h1>
                     <ul className="list-disc pl-4">
                         {usecases.map((usecase, index) => (
-                            <div className="border-b-4 border-black-400 border-opacity-100 border-color:black;">
-                                <ul
-                                    key={usecase?.index}
-                                    className={`cursor-pointer hover:bg-gray-200 px-4 py-6 ${index === 0 ? '' : ''}`}
+                            <div
+                                key={index}
+                                className="border-b-4 border-black-400 border-opacity-100 border-color:black;"
+                            >
+                                <li
+                                    className={`cursor-pointer flex justify-between items-center px-4 py-6 ${selectedUsecase === index ? 'bg-gray-200' : ''}`}
                                     onClick={() => handleUsecaseClick(index)}
                                 >
-                                    {usecase?.heading}
-                                </ul>
+                                    {usecase.heading}
+                                    {selectedUsecase === index && (
+                                        <span className="ml-2 w-3 h-3 rounded-full bg-black"></span>
+                                    )}
+                                </li>
                             </div>
                         ))}
                     </ul>
