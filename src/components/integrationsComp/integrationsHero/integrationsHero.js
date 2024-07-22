e0ce68cace9a8ab5578f97b87c5b90c529777324
prakharlowanshi-3630 <prakharlowanshi@gmail.com>
2024-07-22 14:10:53 +0530

    PRAKHAR || HEADER COLOR AT APP

--- src/components/integrationsComp/integrationsHero/integrationsHero.js
+++ src/components/integrationsComp/integrationsHero/integrationsHero.js
@@ -27,7 +27,7 @@ export default function IntegrationsHero({ combinationData, pluginData }) {
                 <div style={{ backgroundColor: `${plugins[0]?.brandcolor}` }} className="py-12">
                     <div className={`container flex flex-col md:gap-12 gap-6`}>
                         <div
-                            className={`${styles.plugin_name} md:px-8 md:p-3 py-1 px-4 rounded-md  flex items-center gap-8 w-fit`}
+                            className={`${styles.plugin_name} md:px-8 md:p-3 py-1 px-4 rounded-md  flex items-center gap-8 w-fit shadow-black shadow-sm`}
                         >
                             {plugins.map((plug, index) => {
                                 return (
