28bde2a78df696f54451bbcdc91e7d303fc7af27
piyusssshh <piyush@whozzat.com>
2024-08-02 16:35:24 +0530

    PIYUSSSSHH || embed page design done

--- src/pages/embed.js
+++ src/pages/embed.js
@@ -94,6 +94,10 @@ const embed = ({ getStartedData, metaData, faqData, useCases }) => {
         },
     ];
 
+    const openChatWidget = () => {
+        window.chatWidget.open();
+    };
+
     return (
         <>
             <div className="">
@@ -141,37 +145,37 @@ const embed = ({ getStartedData, metaData, faqData, useCases }) => {
                     </div>
                 </div>
                 <div className="bg-white py-12">
-                    <div className="container mx-auto flex flex-col md:flex-row ">
-                        {/* Image Section */}
-                        <div className="flex-1 flex items-center justify-center bg-white p-0 rounded-lg">
-                            <img
-                                src={images[hoveredTag] || '/assets/img/embedfeature/mainflow.svg'}
-                                alt={hoveredTag || 'Default Image'}
-                                className="max-w-full max-h-full object-contain"
-                            />
-                        </div>
-
-                        {/* Tags Section */}
-                        <div className="flex-1 p-0 md:p-8 overflow-y-auto">
-                            <ul className="gap-y-4">
-                                {tags.map((tag) => (
-                                    <li
-                                        key={tag?.id}
-                                        className={`cursor-pointer p-4 rounded border-b-4 border-gray-200 transition-all duration-300 flex items-center ${
-                                            hoveredTag === tag?.id
-                                                ? 'bg-gradient-to-r from-white via-Ivory-100 via-Ivory-300 to-Ivory-500 text-black shadow-lg'
-                                                : 'bg-[#F6F4EE] text-gray-700 hover:bg-gray-300'
-                                        }`}
-                                        onMouseEnter={() => setHoveredTag(tag?.id)}
-                                        onMouseLeave={() => setHoveredTag(null)}
-                                    >
-                                        <span className="flex-1 text-left md:text-center">{tag?.label}</span>
-                                        {hoveredTag === tag?.id && (
-                                            <span className="gap-2 w-3 h-3 rounded-full bg-black"></span>
-                                        )}
-                                    </li>
-                                ))}
-                            </ul>
+                    <div className="container flex flex-col gap-9">
+                        <h2 className="text-3xl font-bold">Features</h2>
+                        <div className=" flex flex-col md:flex-row gap-8 ">
+                            <div className="flex-1">
+                                <ul className="gap-y-4">
+                                    {tags.map((tag) => (
+                                        <li
+                                            key={tag?.id}
+                                            className={`cursor-pointer p-4  border-b-2 border-gray-200 transition-all duration-300 flex items-center ${
+                                                hoveredTag === tag?.id
+                                                    ? 'bg-gradient-to-r from-white via-Ivory-100 via-Ivory-300 to-Ivory-500 text-black shadow-lg'
+                                                    : 'bg-[#F6F4EE] text-gray-700 hover:bg-gray-300'
+                                            }`}
+                                            onMouseEnter={() => setHoveredTag(tag?.id)}
+                                            onMouseLeave={() => setHoveredTag(null)}
+                                        >
+                                            <span className="flex-1 ">{tag?.label}</span>
+                                            {hoveredTag === tag?.id && (
+                                                <span className="gap-2 w-3 h-3 rounded-full bg-black"></span>
+                                            )}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="flex-1 flex items-center justify-center bg-white p-0 rounded-lg">
+                                <img
+                                    src={images[hoveredTag] || '/assets/img/embedfeature/mainflow.svg'}
+                                    alt={hoveredTag || 'Default Image'}
+                                    className="max-w-full max-h-full object-contain"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -208,45 +212,46 @@ const embed = ({ getStartedData, metaData, faqData, useCases }) => {
                             </p>
                         </div>
                         <div className="flex flex-wrap gap-4">
-                            <button className="btn btn-accent btn-md ">Talk to Us</button>
-                            <button className=" btn btn-accent btn-outline btn-md">SignUp & Get Started</button>
-                            <Link href="/faq" className="btn btn-md text-accent bg-transparent border-none">
-                                FAQ
+                            <button className="btn btn-accent btn-md " onClick={openChatWidget}>
+                                Talk to Us
+                            </button>
+
+                            <Link href="/signup" target="_blank">
+                                <button className=" btn btn-accent btn-outline btn-md">SignUp & Get Started</button>
+                            </Link>
+                            <Link href="/faq" target="_blank">
+                                <button className="btn btn-md  bg-transparent border-none">FAQ</button>
                             </Link>
                         </div>
                     </div>
                 </div>
 
                 {/* Use Cases Section */}
-                <div className="bg-[#F6F4EE] py-12">
-                    <div className="container ">
-                        <h1 className="text-3xl font-bold gap-y-4 gap-8">Usecases</h1>
+                <div className="container my-24 flex flex-col gap-9">
+                    <h1 className="text-3xl font-bold gap-y-4 gap-8">Usecases</h1>
+                    <div className="flex flex-col gap-24">
                         {useCases &&
                             useCases.length > 0 &&
                             useCases.map((item, index) => (
-                                <div className="flex flex-col md:flex-row gap-y-4" key={index}>
-                                    {/* Content Section */}
-                                    <div className="flex flex-col gap-y-3 w-full md:w-1/2 p-0 md:p-2">
-                                        <div className="bg-white p-2 rounded-lg  grid gap-y-8">
+                                <div className="flex flex-col md:flex-row" key={index}>
+                                    <div className="flex flex-col p-12 gap-6 bg-white md:w-1/2 rounded">
+                                        <div className="flex flex-col gap-2">
                                             <h1 className="text-xl font-bold gap-y-2">
                                                 {item?.usecase || 'Use Case Title Missing'}
                                             </h1>
                                             <p className="flex flex-col gap-y-3">
                                                 {item?.description || 'Description not available'}
                                             </p>
-                                            {item?.embedusecase?.features?.length > 0 &&
-                                                item?.embedusecase?.features.map((feature, index2) => (
-                                                    <div key={index2} className="bg-[#F6F4EE] p-4 gap-3 rounded-lg">
-                                                        <h2 className="text-lg font-semibold gap-3">
-                                                            {feature?.title || 'Feature Title Missing'}
-                                                        </h2>
-                                                        <p>
-                                                            {feature?.description ||
-                                                                'Feature description not available'}
-                                                        </p>
-                                                    </div>
-                                                ))}
                                         </div>
+                                        {item?.embedusecase?.features?.length > 0 &&
+                                            item?.embedusecase?.features.map((feature, index2) => (
+                                                <div key={index2} className="bg-[#F6F4EE] p-4 gap-3 rounded-lg">
+                                                    <h2 className="text-lg font-semibold gap-3">
+                                                        {feature?.title || 'Feature Title Missing'}
+                                                    </h2>
+                                                    <p>{feature?.description || 'Feature description not available'}</p>
+                                                </div>
+                                            ))}
                                     </div>
 
                                     {/* Image Section */}
