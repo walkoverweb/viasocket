d70b870ffa7f377c79122abd01889db950d305eb
piyusssshh <piyush@whozzat.com>
2024-08-22 10:51:41 +0530

    PIYUSSSSHH || blog post env issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,7 @@ const Index = ({
     navData,
     footerData,
 }) => {
+    console.log('🚀 ~ posts:', posts);
     const formattedIndustries = Industries.industries.map((name, id) => ({ name, id: id + 1 }));
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedCategory, setSelectedCategory] = useState('');
@@ -456,10 +457,10 @@ export async function getServerSideProps() {
 
     const tag = 'via-socket';
     const defaultTag = 'integrations';
-    // const res = await axios.get(
-    //     `${process.env.NEXT_PUBLIC_BASE_URL}/api/fetch-posts?tag=${tag}&defaulttag=${defaultTag}`
-    // );
-    // const posts = await res.data;
+    const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/fetch-posts?tag=${tag}&defaulttag=${defaultTag}`
+    );
+    const posts = await res.data;
     const combos = await fetchCombos(['slack', 'airtable', 'workspace91'], 'All');
     return {
         props: {
@@ -471,7 +472,7 @@ export async function getServerSideProps() {
             faqData: results[5]?.data?.rows,
             navData: results[6]?.data?.rows,
             footerData: results[7]?.data?.rows,
-            // posts: posts,
+            posts: posts,
             combos,
         },
     };
