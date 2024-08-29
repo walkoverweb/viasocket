973fee61a99fce540afd5be12ed8c1d5075fba53
vanshika <guptavanshika9630@gmail.com>
2024-08-29 17:48:52 +0530

    Blog open on onclick on whole card final

--- src/components/blogGrid/blogGrid.js
+++ src/components/blogGrid/blogGrid.js
@@ -7,45 +7,45 @@ const BlogGrid = ({ posts }) => {
     return (
         <>
             {' '}
-            <Link href="/blog" target="_blank">
-                <div className="flex flex-col gap-9">
-                    <h2 className="md:text-6xl text-4xl font-medium">
-                        Know More About <br />
-                        Viasocket Integrations
-                    </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
-                        {posts.map((post, index) => (
-                            <CardComponent key={index} card={post} />
-                        ))}
-                    </div>
-                    <div className="flex justify-center ">
-                        <Link href="/blog" target="_blank" className="btn btn-accent border border-1">
-                            Read More Blogs
-                        </Link>
-                    </div>
+            <div className="flex flex-col gap-9">
+                <h2 className="md:text-6xl text-4xl font-medium">
+                    Know More About <br />
+                    Viasocket Integrations
+                </h2>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
+                    {posts.map((post, index) => (
+                        <CardComponent key={index} card={post} />
+                    ))}
                 </div>
-            </Link>
+                <div className="flex justify-center ">
+                    <Link href="/blog" target="_blank" className="btn btn-accent border border-1">
+                        Read More Blogs
+                    </Link>
+                </div>
+            </div>
         </>
     );
 };
 const CardComponent = ({ card }) => {
     return (
-        <Link href={`/blog/${card?.slug}`}>
-            <div className="card card-compact bg-white-100 w-70  hover:shadow-2xl bg-neutral">
-                <figure className="h-60">
-                    <Image
-                        width={720}
-                        height={720}
-                        src={process.env.NEXT_PUBLIC_BASE_URL + card?.thumbnail}
-                        alt={card?.title}
-                    />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">{card?.title}</h2>
-                    <p className="text-base">{card?.description}</p>
-                    <div className="card-actions justify-start">
-                        <LinkButton href={`/blog/${card?.slug}`} title="Read more" />
-                    </div>
+        <Link
+            href={`/blog/${card?.slug}`}
+            target="_blank"
+            className="card card-compact bg-white-100 w-70  hover:shadow-2xl bg-neutral"
+        >
+            <figure className="h-60">
+                <Image
+                    width={720}
+                    height={720}
+                    src={process.env.NEXT_PUBLIC_BASE_URL + card?.thumbnail}
+                    alt={card?.title}
+                />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{card?.title}</h2>
+                <p className="text-base">{card?.description}</p>
+                <div className="card-actions justify-start">
+                    <LinkButton href={`/blog/${card?.slug}`} title="Read more" />
                 </div>
             </div>
         </Link>
