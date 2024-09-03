579698c718b3a4739c2e857a9e0265a049327292
vanshika <guptavanshika9630@gmail.com>
2024-09-03 14:29:42 +0530

    Some changes in blog

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -59,9 +59,7 @@ export default function TestPage({
                 <div className="flex flex-col gap-4 mt-6 mb-12">
                     <div className="flex items-center justify-between">
                         <button
-
                             className="btn-sm btn border border-1 bg-black text-white hover:bg-black hover:border-black hover:text-white"
-
                             onClick={() => (window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/blog`)}
                             aria-label="back"
                         >
@@ -71,10 +69,8 @@ export default function TestPage({
                         <div className="capitalize text-right">
                             {author}, {date}
                         </div>
-
                     </div>
                     <h1 className="font-medium text-4xl lg:text-4xl text-start lg:text-center ">{title}</h1>
-
 
                     {thumbnailImage !== '' && (
                         <div className="flex justify-center">
@@ -89,9 +85,6 @@ export default function TestPage({
                     )}
                 </div>
                 <div className="body text-xl leading-loose">
-
-
-
                     <MDXRemote {...source} components={component} />
                 </div>
                 {/* <footer className="pt-3 grid gap-4">
@@ -106,7 +99,7 @@ export default function TestPage({
                         </ul>
                     </div>
                 </footer> */}
-
+            </div>
 
             <div className="container py-8">
                 {getStartedData && <GetStarted data={getStartedData} isHero={'false'} />}
