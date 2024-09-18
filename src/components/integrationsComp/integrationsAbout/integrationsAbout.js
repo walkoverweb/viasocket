04c057dee64175205c615f9d783381385d680907
vanshika <guptavanshika9630@gmail.com>
2024-09-18 11:40:39 +0530

    reverse the order of abut text and also background on logog with same logog and primary colour

--- src/components/integrationsComp/integrationsAbout/integrationsAbout.js
+++ src/components/integrationsComp/integrationsAbout/integrationsAbout.js
@@ -8,40 +8,46 @@ export default function IntegrationsAbout({ plugins }) {
             <div className="py-24">
                 <div className="flex lg:flex-row md:flex-row flex-col gap-10 container justify-between">
                     {plugins?.length &&
-                        plugins?.map((plugin, index) => {
-                            return (
-                                <div key={plugin?.rowid || index} className="flex flex-1 flex-col justify-start gap-4">
-                                    <Image
-                                        src={plugin?.iconurl || 'https://placehold.co/40x40'}
-                                        width={34}
-                                        height={34}
-                                        alt={plugin?.name}
-                                    />
-                                    <h6 className="lg:text-[32px] md:text-2xl text-xl font-medium">
-                                        {`About ${plugin?.name}`}
-                                    </h6>
-                                    <p className="md:text-xl text-base">{plugin?.description}</p>
-                                    <div>
-                                        <Link
-                                            href={
-                                                plugin?.domain?.startsWith('http')
-                                                    ? plugin?.domain
-                                                    : 'http://' + plugin?.domain
-                                            }
-                                            target="_blank"
-                                        >
-                                            <button
-                                                className="font-medium text-[#2D81F7] flex items-center"
-                                                aria-label="load more apps"
+                        plugins
+                            ?.slice()
+                            .reverse()
+                            .map((plugin, index) => {
+                                return (
+                                    <div
+                                        key={plugin?.rowid || index}
+                                        className="flex flex-1 flex-col justify-start gap-4"
+                                    >
+                                        <Image
+                                            src={plugin?.iconurl || 'https://placehold.co/40x40'}
+                                            width={34}
+                                            height={34}
+                                            alt={plugin?.name}
+                                        />
+                                        <h6 className="lg:text-[32px] md:text-2xl text-xl font-medium">
+                                            {`About ${plugin?.name}`}
+                                        </h6>
+                                        <p className="md:text-xl text-base">{plugin?.description}</p>
+                                        <div>
+                                            <Link
+                                                href={
+                                                    plugin?.domain?.startsWith('http')
+                                                        ? plugin?.domain
+                                                        : 'http://' + plugin?.domain
+                                                }
+                                                target="_blank"
                                             >
-                                                Learn More
-                                                <MdChevronRight fontSize={22} />
-                                            </button>
-                                        </Link>
+                                                <button
+                                                    className="font-medium text-[#2D81F7] flex items-center"
+                                                    aria-label="load more apps"
+                                                >
+                                                    Learn More
+                                                    <MdChevronRight fontSize={22} />
+                                                </button>
+                                            </Link>
+                                        </div>
                                     </div>
-                                </div>
-                            );
-                        })}
+                                );
+                            })}
                     {plugins?.length == 1 && (
                         <div className="flex flex-1 flex-col gap-4">
                             <Link href="/" aria-label="main link">
--- src/components/integrationsComp/integrationsHero/integrationsHero.js
+++ src/components/integrationsComp/integrationsHero/integrationsHero.js
@@ -101,7 +101,7 @@ export default function IntegrationsHero({ combinationData, pluginData }) {
                                                           src={pluginData[1]?.iconurl}
                                                           width={24}
                                                           height={24}
-                                                          className="h-auto hidden sm:block"
+                                                          className="h-auto hidden sm:block bg-gray-100"
                                                           alt={pluginData[1]?.name}
                                                       />
                                                   )}
@@ -127,7 +127,7 @@ export default function IntegrationsHero({ combinationData, pluginData }) {
                                                           src={pluginData[0]?.iconurl}
                                                           width={24}
                                                           height={24}
-                                                          className="h-auto hidden sm:block"
+                                                          className="h-auto hidden sm:block bg-gray-100"
                                                           alt={pluginData[0]?.name}
                                                       />
                                                   )}
