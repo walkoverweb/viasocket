8d21ba9151bd81a4b8d059dd3911e9ed660a9531
vanshika <guptavanshika9630@gmail.com>
2024-11-20 10:31:33 +0530

    Changes on footer and navbar section

--- src/components/newfooter/newfooter.js
+++ src/components/newfooter/newfooter.js
@@ -46,8 +46,8 @@ const NewFooter = ({ footerData }) => {
         });
     return (
         <>
-            <div className="flex flex-col lg:flex-row sm:flex-row">
-                <div className="grid grid-cols-1 gap-y-8 lg:gap-y-24 w-full justify-between p-6 sm:p-12">
+            <div className="grid md:grid-cols-3 sm:grid-cols-1 lg:grid-cols-4 border border-black border-b border-0">
+                <div className="grid grid-cols-1 gap-y-8 lg:gap-y-24 w-full justify-between p-6 sm:p-12 order-last lg:order-first border border-black border-b-0 border-r-0    border-t-0 border-l md:order-4">
                     <div className="flex flex-col gap-2 mb-12 sm:mb-24 py-4">
                         <Link href="/" aria-label="socket fav icon">
                             <Image
@@ -57,10 +57,10 @@ const NewFooter = ({ footerData }) => {
                                 alt="socket fav icon"
                             />
                         </Link>
-                        <p> © 2024 viaSocket</p>
+                        <p>© 2024 viaSocket</p>
                         <p>All rights reserved.</p>
                     </div>
-                    <div className="flex items-end gap-2 w-full flex-wrap my-8 ">
+                    <div className="flex items-end gap-2 w-full flex-wrap my-8">
                         <span>A product of</span>
                         <Link href="https://walkover.in/" target="_blank" aria-label="walkover">
                             <Image src="/assets/brand/walkover.svg" alt="walkover" width={100} height={20} />
@@ -68,15 +68,15 @@ const NewFooter = ({ footerData }) => {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-1 gap-4 lg:gap-2 w-full border-l border-black p-6 sm:p-16">
+                <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-4 w-full border-l border-b border-black p-6 sm:p-16  md:order-1">
                     {renderedGroups.slice(0, 2)}
                 </div>
 
-                <div className="grid grid-cols-1 gap-4 lg:gap-8 w-full border-l border-black p-6 sm:p-16">
+                <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-4 w-full border-l border-black p-6 sm:p-16 min-[768px]:border-l min-[1024px]:border-l md:order-2  border-b min-[1024]:border-b-0 ">
                     {renderedGroups.slice(2, 4)}
                 </div>
 
-                <div className="grid grid-cols-1 gap-4 lg:gap-8 w-full border-l border-black p-6 sm:p-16">
+                <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-4 w-full  min-[768px]:border-l border-black p-6 sm:p-16 border-b min-[1024]:border-b-0  border-l min-[768px]:border-t-0 md:order-3">
                     {renderedGroups.slice(4, 6)}
                 </div>
             </div>
--- src/components/newnavbar/newnavbar.js
+++ src/components/newnavbar/newnavbar.js
@@ -1,147 +1,135 @@
-import { MdMenu } from 'react-icons/md';
+import { MdMenu, MdLogin, MdPersonAdd } from 'react-icons/md';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './newnavbar.module.scss';
-import NotificationBar from '../notificationBar/notificationbar';
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 
 const NewNavbar = ({ navData, utm }) => {
-    var shorterData;
+    let shorterData;
     if (navData?.length > 0) {
         shorterData = navData?.sort((a, b) => {
             return parseInt(a.priority) - parseInt(b.priority);
         });
     }
+
     return (
         <>
-            <div className={`${styles.navbar_cont}container  flex w-full flex-col mt-12`}>
-                <div className={`${styles.navbar} flex justify-between items-center w-full container gap-4`}>
-                    <div className="w-auto lg:w-[165px]  sm:w-[80px] h-[56px] border border-black border-b-0 p-2 flex items-center justify-center px-4 sm:px-6 md:px-8 lg:px-20">
-                        <Link className="flex flex-row gap-2 items-center justify-center" href="/" aria-label="logo">
-                            <Image
-                                className="h-[24px] sm:h-[16px] w-[24px] sm:w-[16px]"
-                                src={'/assets/brand/favicon_dark.svg'}
-                                width={32}
-                                height={32}
-                                alt="viasocket"
-                            />
-                            <span className="flex items-center font-normal text-xs sm:text-sm  lg:text-lg">SOCKET</span>
-                        </Link>
+            <div className={`${styles.navbar_cont} container flex flex-col w-full mt-12`}>
+                <div className="flex items-center justify-between w-full  ">
+                    <div className=" border border-black border-b-0">
+                        <div className="flex items-center p-2 ">
+                            <Link href="/" aria-label="logo" className="flex items-center gap-2">
+                                <Image
+                                    src="/assets/brand/favicon_dark.svg"
+                                    width={32}
+                                    height={32}
+                                    alt="viasocket"
+                                    className="h-6 w-6"
+                                />
+                                <span className="text-lg font-semibold hidden sm:inline font-times-now text-lg font-normal">
+                                    SOCKET
+                                </span>
+                            </Link>
+                        </div>
                     </div>
 
-                    <div className="lg:flex hidden menu-horizontal ">
+                    <div className="hidden lg:flex items-center flex-1 justify-end ">
                         {shorterData &&
-                            shorterData.map((option, index) => {
-                                if (option.group_name === null && option.is_mininavonly === null) {
-                                    if (option.is_parent) {
-                                        return (
-                                            <div className="dropdown dropdown-bottom w-[165px]" key={index}>
-                                                <div
-                                                    tabIndex={0}
-                                                    role="button"
-                                                    className="btn h-[56px] w-full normal-case justify-between text-black font-times-now text-lg font-semibold border  border-black bg-white hover:bg-gray-50 text-md md:text-lg"
-                                                    aria-label="nav option"
-                                                >
-                                                    <span>{option?.name}</span>
-                                                    <MdOutlineKeyboardArrowDown size={20} />
-                                                </div>
-                                                <ul
-                                                    tabIndex={0}
-                                                    className="dropdown-content menu shadow bg-base-100 w-full"
-                                                >
-                                                    {shorterData.map((child, childIndex) => {
-                                                        if (child.group_name && child.group_name === option.name) {
-                                                            return (
-                                                                <li key={childIndex}>
-                                                                    {child.name === 'Live Chat' ? (
-                                                                        <button
-                                                                            onClick={openChatWidget}
-                                                                            className="btn h-[56px] normal-case text-black font-times-now text-lg font-normal"
-                                                                            aria-label="Chat"
-                                                                        >
-                                                                            Live Chat
-                                                                        </button>
-                                                                    ) : (
-                                                                        <Link
-                                                                            href={`${child.link ? child.link : ''}`}
-                                                                            className="btn h-[56px] normal-case text-black font-times-now text-lg font-normal"
-                                                                            target="_blank"
-                                                                            aria-label="logo"
-                                                                        >
-                                                                            {child.name}
-                                                                        </Link>
-                                                                    )}
-                                                                </li>
-                                                            );
-                                                        }
-                                                    })}
-                                                </ul>
-                                            </div>
-                                        );
-                                    } else {
-                                        return (
-                                            <div className="w-[165px] " key={index}>
-                                                <Link
-                                                    href={`${option.link ? option.link : '#'}`}
-                                                    className="btn h-[56px] w-full normal-case text-black font-times-now text-lg font-normal border border-r-0 border-b-0  border-black bg-white hover:bg-gray-50 text-md md:text-lg"
-                                                    target={option?.name?.toLowerCase() === 'home' ? '' : '_blank'}
-                                                    aria-label={option?.name}
-                                                >
-                                                    {option?.name}
-                                                </Link>
-                                            </div>
-                                        );
-                                    }
-                                }
-                            })}
-
-                        <div className="lg:w-[165px]">
-                            <Link
-                                href={`/login?utm_source=${utm}`}
-                                className="btn h-[56px] w-full normal-case text-black font-times-now text-lg font-normal border border-r-0 border-b-0 border-black bg-white hover:bg-gray-50 text-md md:text-lg"
-                            >
-                                Login
-                            </Link>
-                        </div>
-
-                        <div className="w-[165px]">
-                            <Link
-                                href={`/signup?utm_source=${utm}`}
-                                className="btn h-[56px] w-full normal-case text-white font-times-now text-lg font-normal border border-black border-b-0 bg-red-700 hover:bg-red-800 text-md md:text-lg"
-                            >
-                                Sign Up
-                            </Link>
-                        </div>
+                            shorterData.map((option, index) => (
+                                <div key={index} className="relative">
+                                    {option.is_parent ? (
+                                        <div className="dropdown dropdown-hover">
+                                            <button
+                                                tabIndex={0}
+                                                className="btn px-4 py-2 text-base text-black bg-white border border-black flex items-center justify-between border min-[377px]:border-b-0 border-b border-black"
+                                            >
+                                                {option.name}
+                                                <MdOutlineKeyboardArrowDown size={20} />
+                                            </button>
+                                            <ul
+                                                tabIndex={0}
+                                                className="dropdown-content menu shadow bg-white border border-gray-200 w-48"
+                                            >
+                                                {shorterData.map((child, childIndex) =>
+                                                    child.group_name === option.name ? (
+                                                        <li key={childIndex}>
+                                                            <Link
+                                                                href={child.link || '#'}
+                                                                className="text-black text-sm hover:bg-gray-100 px-2 py-1 block"
+                                                            >
+                                                                {child.name}
+                                                            </Link>
+                                                        </li>
+                                                    ) : null
+                                                )}
+                                            </ul>
+                                        </div>
+                                    ) : (
+                                        <Link
+                                            href={option.link || '#'}
+                                            className="btn px-4 py-2 text-base text-black bg-white border min-[377px]:border-b-0 min-[377px]:border-r-0 border-b border-black font-times-now text-lg font-normal"
+                                        >
+                                            {option.name}
+                                        </Link>
+                                    )}
+                                </div>
+                            ))}
                     </div>
 
-                    <div className="dropdown dropdown-end lg:hidden block">
-                        <div
-                            tabIndex={0}
-                            role="button"
-                            className="w-[165px] h-[56px] flex items-center justify-center border min-[377px]:border-b-0 border-b border-black"
-                            aria-label="dropdown nav"
+                    <div className="flex items-center ">
+                        <Link
+                            href={`/login?utm_source=${utm}`}
+                            className="hidden lg:inline-block btn px-4 py-2 text-black  bg-white font-times-now text-lg font-normal border border-r-0 border-b-0 border-black bg-white hover:bg-gray-50 text-md md:text-lg"
                         >
-                            <MdMenu className="w-[24px] h-[24px]" />
+                            Login
+                        </Link>
+                        <Link
+                            href={`/signup?utm_source=${utm}`}
+                            className="hidden lg:inline-block btn px-4 py-2  text-white font-times-now text-lg font-normal border border-black border-b-0 bg-red-700 hover:bg-red-800 text-md md:text-lg"
+                        >
+                            Sign Up
+                        </Link>
+
+                        <Link
+                            href={`/login?utm_source=${utm}`}
+                            className="lg:hidden text-black p-2 rounded-md border border-black"
+                            aria-label="Login"
+                        >
+                            <MdLogin size={24} />
+                        </Link>
+                        <Link
+                            href={`/signup?utm_source=${utm}`}
+                            className="lg:hidden text-white p-2 rounded-md bg-red-700 hover:bg-red-800 border border-black"
+                            aria-label="Sign Up"
+                        >
+                            <MdPersonAdd size={24} />
+                        </Link>
+
+                        <div className="dropdown dropdown-end lg:hidden">
+                            <button
+                                tabIndex={0}
+                                className="p-2 border border-black bg-white rounded-md"
+                                aria-label="Menu"
+                            >
+                                <MdMenu size={24} />
+                            </button>
+                            <ul
+                                tabIndex={0}
+                                className="dropdown-content menu p-2 shadow bg-white border border-gray-300 w-48 "
+                            >
+                                {shorterData &&
+                                    shorterData.map((option, index) => (
+                                        <li key={index}>
+                                            <Link
+                                                href={option.link || '#'}
+                                                className="text-black text-sm hover:bg-gray-100 block px-2 py-1"
+                                            >
+                                                {option.name}
+                                            </Link>
+                                        </li>
+                                    ))}
+                            </ul>
                         </div>
-                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 bg-base-100 w-[165px] shadow-lg">
-                            {shorterData &&
-                                shorterData.map((option, index) => {
-                                    if (!option.is_parent) {
-                                        return (
-                                            <li key={index}>
-                                                <Link
-                                                    href={`${option.link ? option.link : '#'}`}
-                                                    target="_blank"
-                                                    aria-label={option?.name}
-                                                    className="text-black font-times-now text-lg font-normal"
-                                                >
-                                                    {option?.name}
-                                                </Link>
-                                            </li>
-                                        );
-                                    }
-                                })}
-                        </ul>
                     </div>
                 </div>
             </div>
