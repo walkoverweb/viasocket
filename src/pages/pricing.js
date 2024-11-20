2fb713c6becf29076998152775f9e7c9b15a7ded
vanshika <guptavanshika9630@gmail.com>
2024-11-20 17:53:14 +0530

    New pricing page

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -26,26 +26,28 @@ export async function getServerSideProps() {
 const pricing = ({ pathArray, navData, footerData, faqData, betterChoice, metaData, getStartedData, faqName }) => {
     const [selectedIndex, setSelectedIndex] = React.useState(0);
     const [isToggled, setIsToggled] = useState(false);
+
     return (
         <>
             <NewNavbar navData={navData} utm={'pricing'} />
-            <div className="container flex flex-col justify-center gap-6 mb-12 md:mb-24">
-                <div className="border-2 border-black">
-                    <div className="h-24 bg-gradient-to-r from-green-300 to-green-200 to-pink-200 to-pink-100"></div>
-                    <div className="grid grid-cols-1 md:grid-cols-2 ">
-                        <div className="flex flex-col gap-8 p-12 bg-gradient-to-r from-green-300 to-green-200 to-pink-200 to-pink-100">
-                            <h1 className="font-bold text-4xl md:text-6xl overflow-hidden">
-                                Simple Pricing For Powerful Automation
+            <div className="container flex flex-col justify-center gap-6 mb-12 md:mb-24 ">
+                <div className="border border-black gradient-background">
+                    <div className="h-28 "></div>
+                    <div className="grid grid-cols-1 lg:grid-cols-2">
+                        <div className=" flex flex-col gap-8 p-12 justify-center ">
+                            <h1 className="heading md:text-6xl overflow-hidden text-60px leading-normal tracking-lose-0p5 font-family-times-now ">
+                                Simple Pricing for Powerful Automation
                             </h1>
-                            <h2 className="text-xl md:text-2xl font-semibold w-full ">
+
+                            <h2 className="text-xl  w-full  text-[252525] font-normal text-normal">
                                 Enjoy a 30-Day Free Trial.
                                 <br />
                                 No credit card required.
                             </h2>
-                            <div className="border-2 border-black p-4 w-full md:w-1/2">
-                                <div className="form-control flex items-center">
-                                    <label className="cursor-pointer flex items-center">
-                                        <span className="font-semibold mx-2 text-xl md:text-2xl">Billed Yearly</span>
+                            <div className="border border-black py-4 w-full lg:w-1/3">
+                                <div className="form-control flex items-center ">
+                                    <label className="cursor-pointer flex flex-row gap-2 items-center">
+                                        <span className="font-semibold text-md  ">BILLED YEARLY </span>
                                         <input
                                             type="checkbox"
                                             className="toggle"
@@ -56,15 +58,21 @@ const pricing = ({ pathArray, navData, footerData, faqData, betterChoice, metaDa
                                 </div>
                             </div>
                         </div>
-                        <div className="grid grid-cols-1 md:grid-cols-2">
-                            <div className="flex flex-col gap-y-8 border border-black font-normal">
-                                <div className="flex flex-col gap-12 my-12 px-6">
-                                    <div className="subheading font-semibold text-3xl md:text-4xl">Starter</div>
-                                    <div className="flex flex-col gap-2">
-                                        <div className="text-4xl md:text-6xl">${isToggled ? 30 * 10 : 30}</div>
-                                        <div className="font-semibold">month/workspace</div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 bg-white ">
+                            <div className="flex flex-col gap-y-2 border border-l-0 min-[1003px]:border-l border-r-0 min-[768px]:border-r border-b-0 border-black font-normal ">
+                                <div className="subheading font-semibold text-md  justify-around mt-12 mx-6">
+                                    {'Starter'.toUpperCase()}
+                                </div>
+                                <div className="flex flex-col gap-8 my-12 px-6">
+                                    <div class="flex flex-col gap-2">
+                                        <p class="text-4xl md:text-6xl font-family-times-now">
+                                            ${isToggled ? 30 * 10 : 30}
+                                        </p>
+                                        <p class="font-normal tracking-wide overflow-hidden">
+                                            ${isToggled ? 'YEAR' : 'MONTH'}/WORKSPACE
+                                        </p>
                                     </div>
-                                    <div className="flex flex-col">
+                                    <div className="flex flex-col font-normal text-normal">
                                         <div>Invocations: 10,000/Month</div>
                                         <div>Execution Time Limit: 30 Seconds</div>
                                         <div>Min. Polling time: 1 Min</div>
@@ -74,20 +82,30 @@ const pricing = ({ pathArray, navData, footerData, faqData, betterChoice, metaDa
                                 </div>
                                 <a
                                     href="/start-trial"
-                                    className="flex justify-center items-center border-t border-black "
+                                    className="flex flex-col items-center mt-auto border-t border-black"
                                 >
-                                    <button className="inline-block text-center p-4 font-bold">Start Free Trial</button>
+                                    <div className="flex justify-center">
+                                        <button className="inline-block text-center p-4 font-semibold text-md ">
+                                            {'Start Free Trial'.toUpperCase()}
+                                        </button>
+                                    </div>
                                 </a>
                             </div>
 
-                            <div className="flex flex-col gap-y-8 border border-black font-normal">
-                                <div className="flex flex-col gap-12 my-12 px-6">
-                                    <div className="subheading font-semibold text-3xl md:text-4xl">Team</div>
+                            <div className="flex flex-col gap-y-2 border-t border-black font-normal">
+                                <div className="subheading font-semibold text-md  justify-around mt-12 mx-6">
+                                    {'Team'.toUpperCase()}
+                                </div>
+                                <div className="flex flex-col gap-8 my-12 px-6">
                                     <div className="flex flex-col gap-2">
-                                        <div className="text-4xl md:text-6xl">${isToggled ? 60 * 10 : 60}</div>
-                                        <div className="font-semibold">month/workspace</div>
+                                        <div className="text-4xl md:text-6xl font-family-times-now">
+                                            ${isToggled ? 60 * 10 : 60}
+                                        </div>
+                                        <div class="font-normal tracking-wide overflow-hidden">
+                                            ${isToggled ? 'YEAR' : 'MONTH'}/WORKSPACE
+                                        </div>
                                     </div>
-                                    <div className="flex flex-col">
+                                    <div className="flex flex-col font-normal text-normal">
                                         <div>Invocations: 10,000/Month</div>
                                         <div>Execution Time Limit: 60 Seconds</div>
                                         <div>Min. Polling time: 1 Min</div>
@@ -97,17 +115,20 @@ const pricing = ({ pathArray, navData, footerData, faqData, betterChoice, metaDa
                                 </div>
                                 <a
                                     href="/start-trial"
-                                    className="flex justify-center items-center border-t border-black bg-black text-white"
+                                    className="flex justify-center items-center border-t  mt-auto border-black bg-black text-white"
                                 >
-                                    <button className="inline-block text-center p-4 font-bold">Start Free Trial</button>
+                                    <button className="inline-block text-center p-4 font-semibold text-md ">
+                                        {' '}
+                                        {'Start Free Trial'.toUpperCase()}
+                                    </button>
                                 </a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="container flex flex-col justify-center gap-6 md:gap-16 my-12 md:my-24">
-                <div className="border-2 border-black p-6 md:p-12 flex flex-col gap-4 md:gap-8">
+            {/* <div className="container flex flex-col justify-center gap-6 md:gap-16 my-24 md:my-48">
+                <div className="border border-black p-6 md:p-12 flex flex-col gap-4 md:gap-8">
                     <div className="text-3xl md:text-4xl lg:text-6xl xl:w-1/2">
                         Explore Hundreds of Features, Available on Every Plan
                     </div>
@@ -115,19 +136,19 @@ const pricing = ({ pathArray, navData, footerData, faqData, betterChoice, metaDa
                         Get unrestricted access to all features, no matter your choice of plan.
                     </div>
                     <div className="flex justify-start">
-                        <button className="btn btn-active bg-red-700 text-white text-xs sm:text-sm md:text-base lg:text-lg px-4 py-2 hover:bg-red-800 hover:text-white">
+                        <button className="btn btn-active bg-red-700 text-white text-xs sm:text-sm md:text-base lg:text-lg px-4 py-2 hover:bg-black hover:text-white">
                             SEE ALL FEATURES
                         </button>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
-            <div className="container flex flex-col justify-center gap-6 md:gap-16 my-12 md:my-24">
-                <h2 className="text-3xl md:text-4xl lg:text-6xl md:w-2/3 font-normal px-6 md:px-12">
-                    What Makes <span className="text-red-700">Viasocket</span> A Better Choice
+            <div className="container flex flex-col justify-center gap-6 md:gap-8 my-24 md:my-48">
+                <h2 className="text-3xl md:text-4xl lg:text-6xl md:w-2/3 font-normal font-family-times-now">
+                    What makes <br /> <span className="text-red-700 italic">viaSocket</span> a better choice ?
                 </h2>
 
-                <div className="flex flex-col lg:flex-row border-2 border-black">
+                <div className="flex flex-col lg:flex-row border border-black">
                     <div className="flex flex-col w-full lg:w-1/2 py-12 md:py-24 px-6 md:px-12 text-base md:text-xl gap-4">
                         {betterChoice.length > 0 &&
                             betterChoice.map((choice, index) => (
@@ -137,35 +158,68 @@ const pricing = ({ pathArray, navData, footerData, faqData, betterChoice, metaDa
                                     onClick={() => setSelectedIndex(index)}
                                 >
                                     <div>{choice.name}</div>
+
+                                    {selectedIndex === index && (
+                                        <div
+                                            className="lg:hidden mt-2 text-base md:text-lg text-gray-700"
+                                            style={{
+                                                backgroundImage: `url('/assets/img/pricing.png')`,
+                                                backgroundSize: 'cover',
+                                                backgroundPosition: 'center',
+                                            }}
+                                        >
+                                            <div className=" md:p-8 mx-4 md:mx-24">
+                                                <p className="text-base md:text-lg text-white  p-4">
+                                                    {choice.description}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    )}
                                 </div>
                             ))}
                     </div>
-                    <div className="flex flex-col w-full lg:w-1/2 bg-gray-200 py-12 md:py-24 px-6 md:px-12">
-                        <p className="text-base md:text-lg">{betterChoice[selectedIndex]?.description}</p>
+
+                    <div
+                        className="lg:flex hidden w-full lg:w-1/2 md:py-12 px-6 md:px-12 bg-opacity-200 "
+                        style={{
+                            backgroundImage: `url('/assets/img/pricing.png')`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                        }}
+                    >
+                        <div className="p-12 h-full flex items-center justify-center">
+                            <p className="text-base md:text-lg text-white">
+                                {betterChoice[selectedIndex]?.description}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="container flex flex-col justify-center gap-6 md:gap-16 my-12 md:my-24">
-                <div className="border-2 border-black p-6 md:p-12 flex flex-col gap-4 md:gap-8">
-                    <div className="text-3xl md:text-4xl lg:text-6xl xl:w-1/2">
-                        Free Services for Impactful Organizations
+            <div className="container flex flex-col justify-center  gap-6 md:gap-16 my-24 md:my-48">
+                <div className=" border border-t-0 border-black">
+                    <div className="flex items-center justify-center sm:-mt-5 md:-mt-5 lg:-mt-8">
+                        <div className="border-t border-black flex-grow" />
+                        <div className="text-3xl md:text-4xl lg:text-6xl px-4 font-family-times-now text-center">
+                            Free Services for Impactful Organizations
+                        </div>
+                        <div className="border-t border-black flex-grow" />
                     </div>
-                    <div className="text-base md:text-lg xl:w-1/2">
-                        We support organizations driving change with free access to our automation solutions
-                    </div>
-                    <div className="flex justify-start">
-                        <button className="btn btn-active bg-red-700 text-white text-xs sm:text-sm md:text-base lg:text-lg px-4 py-2 hover:bg-red-800 hover:text-white">
-                            CLICK HERE
-                        </button>
+
+                    <div className="flex flex-col gap-4 md:gap-8 items-center text-center py-16 border-black">
+                        <div className="text-base md:text-lg xl:w-1/2 text-center">
+                            We support organizations driving change with free access to our automation solutions
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="btn btn-active bg-red-700 text-white  px-20 py-2 hover:bg-black hover:text-white font-semibold text-md">
+                                CLICK HERE
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div className="container flex flex-col justify-center  md:gap-16 gap-6 my-12 md:my-24 ">
-                <div className="flex flex-col border-2 border-black">
-                    <h2 className="md:text-4xl sm:text-3xl lg:text-6xl  font-normal px-12 mt-24 overflow-hidden">
-                        Trendy Questions
-                    </h2>
+            <div className="container flex flex-col justify-center  my-24 md:mt-48 ">
+                <div className="flex flex-col border border-black">
                     <div className="flex flex-col text-xl">
                         <div className="bg-white">
                             {faqData && faqData.length > 0 && (
@@ -176,10 +230,7 @@ const pricing = ({ pathArray, navData, footerData, faqData, betterChoice, metaDa
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div className="container flex flex-col justify-center  md:gap-16 gap-6 my-12 md:my-24 ">
-                <div className="border-2 border-black  flex flex-col gap-8 ">
+                <div className="border border-black border-l-0 border-b-0  border-t-0 flex flex-col gap-8 ">
                     <NewFooter footerData={footerData} />
                 </div>
             </div>
