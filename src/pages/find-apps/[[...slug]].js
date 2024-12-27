420e06fcabb3b05db4a43ee3cdd2acb764d372df
piyusssshh <piyush@whozzat.com>
2024-12-27 16:40:46 +0530

    VSWEB-197 | added meta title for find apps

--- src/pages/find-apps/[[...slug]].js
+++ src/pages/find-apps/[[...slug]].js
@@ -1,89 +1,105 @@
 import AlphabeticalComponent from '@/components/alphabetSort/alphabetSort';
+import AlphabetComp from '@/components/aZComp/alphabetComp/alphabetComp';
+import AZMetaHeadComp from '@/components/aZComp/aZMetaHeadComp/aZMetaHeadComp';
+import Head from 'next/head';
 import Image from 'next/image';
 
 const SelectedAlphabetPage = ({ apps, alphabet, appDetails, step, alphabet2 }) => {
     return (
         <div className="container py-20 gap-8 ">
             {step == 0 && (
-                <div className="flex flex-col gap-8 items-center">
+                <>
+                    <AZMetaHeadComp alphabet={alphabet} />
                     <div className="flex flex-col gap-8 items-center">
-                        <h1 className="text-center lg:text-2xl md:text-xl text-lg font-semibold pb-4">
-                            Browse apps by name - {alphabet?.toUpperCase() || '-'}
-                        </h1>
+                        <div className="flex flex-col gap-8 items-center">
+                            <h1 className="text-center lg:text-2xl md:text-xl text-lg font-semibold pb-4">
+                                Browse apps by name - {alphabet?.toUpperCase() || '-'}
+                            </h1>
 
-                        <AlphabeticalComponent alphabet={alphabet} appDetails={appDetails} step={step} />
-                    </div>
+                            <AlphabetComp alphabet={alphabet} appDetails={appDetails} step={step} />
+                        </div>
 
-                    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 justify-center items-center py-4">
-                        {apps?.map((app) => (
-                            <a
-                                key={app?.rowid}
-                                href={app?.appslugname ? `/find-apps/${alphabet}/${app?.appslugname}` : `/noplugin`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="block"
-                                aria-label="app"
-                            >
-                                <p className="text-base py-1 ">{app.name}</p>
-                            </a>
-                        ))}
+                        <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 justify-center items-center py-4">
+                            {apps?.map((app) => (
+                                <a
+                                    key={app?.rowid}
+                                    href={app?.appslugname ? `/find-apps/${alphabet}/${app?.appslugname}` : `/noplugin`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="block"
+                                    aria-label="app"
+                                >
+                                    <p className="text-base py-1 ">{app.name}</p>
+                                </a>
+                            ))}
+                        </div>
                     </div>
-                </div>
+                </>
             )}
             {step == 1 && (
-                <div className="w-full  flex flex-col gap-8 items-center">
-                    <div className="flex flex-col gap-8 items-center">
-                        <h1 className="text-center lg:text-2xl md:text-xl text-lg font-semibold pb-4">
-                            Browse apps by name - {alphabet?.toUpperCase() || '-'}
-                        </h1>
+                <>
+                    <Head>
+                        <title>Apps starting with {alphabet?.toUpperCase()}</title>
+                        <meta
+                            name="description"
+                            content={`Browse and discover a wide range of apps that start with the letter ${alphabet?.toUpperCase()}. Find the perfect app for your needs today!`}
+                        />
+                        <meta property="og:title" content={`Apps starting with ${alphabet?.toUpperCase()}`} />
+                        <meta
+                            property="og:description"
+                            content={`Browse and discover a wide range of apps that start with the letter ${alphabet?.toUpperCase()}. Find the perfect app for your needs today!`}
+                        />
+                    </Head>
+                    <div className="w-full  flex flex-col gap-8 items-center">
+                        <div className="flex flex-col gap-8 items-center">
+                            <h1 className="text-center lg:text-2xl md:text-xl text-lg font-semibold pb-4">
+                                Browse apps by name - {alphabet?.toUpperCase() || '-'}
+                            </h1>
 
-                        <AlphabeticalComponent alphabet={alphabet} appDetails={appDetails} step={step} />
-                    </div>
+                            <AlphabetComp alphabet={alphabet} appDetails={appDetails} step={step} />
+                        </div>
 
-                    <div className="w-full  grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 justify-center items-center py-4">
-                        {apps?.map((app) => (
-                            <a
-                                key={app?.rowid}
-                                href={app?.appslugname ? `/find-apps/${alphabet}/${app?.appslugname}` : `/noplugin`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="block"
-                                aria-label="app"
-                            >
-                                <p className=" hover:text-blue-600 text-base py-1 ">{app.name}</p>
-                            </a>
-                        ))}
+                        <div className="w-full  grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 justify-center items-center py-4">
+                            {apps?.map((app) => (
+                                <a
+                                    key={app?.rowid}
+                                    href={app?.appslugname ? `/find-apps/${alphabet}/${app?.appslugname}` : `/noplugin`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="block"
+                                    aria-label="app"
+                                >
+                                    <p className=" hover:text-blue-600 text-base py-1 ">{app.name}</p>
+                                </a>
+                            ))}
+                        </div>
                     </div>
-                </div>
+                </>
             )}
 
             {step == 2 && (
-                <div className="flex flex-col gap-8 items-center">
-                    <h1 className="text-center lg:text-2xl md:text-xl text-lg font-semibold pb-4">
-                        Browse apps by name - {alphabet?.toUpperCase() || '-'} - {appDetails?.name}
-                    </h1>
-                    <a
-                        target="_blank"
-                        href={`/integrations/${appDetails?.appslugname}`}
-                        className="flex gap-2  items-center"
-                    >
-                        <Image
-                            src={appDetails?.iconurl}
-                            className="h-8 w-fit"
-                            width={46}
-                            height={46}
-                            alt={appDetails?.name}
+                <>
+                    <Head>
+                        <title>
+                            Browse Apps {alphabet?.toUpperCase()} - {appDetails?.name} Integration
+                        </title>
+                        <meta
+                            name="description"
+                            content={`Discover and explore Airtable among apps starting with  ${alphabet?.toUpperCase()}. Find the perfect app to integrate with ${appDetails?.name} and automate your workflows. Browse all available integrations here!. Find the perfect app for your needs today!`}
                         />
-                        <span className="text-3xl font-semibold">{appDetails?.name}</span>
-                    </a>
-                    <span className="text-4xl"> + </span>
-                    <AlphabeticalComponent alphabet={alphabet} appDetails={appDetails} step={step} />
-                </div>
-            )}
-
-            {step == 3 && (
-                <div className=" w-full flex flex-col gap-8 items-center">
-                    <div className="flex flex-col gap-6 items-center">
+                        <meta
+                            property="og:title"
+                            content={`Browse Apps ${alphabet?.toUpperCase()} - ${appDetails?.name} Integration`}
+                        />
+                        <meta
+                            property="og:description"
+                            content={`Discover and explore Airtable among apps starting with  ${alphabet?.toUpperCase()}. Find the perfect app to integrate with ${appDetails?.name} and automate your workflows. Browse all available integrations here!. Find the perfect app for your needs today!`}
+                        />
+                    </Head>
+                    <div className="flex flex-col gap-8 items-center">
+                        <h1 className="text-center lg:text-2xl md:text-xl text-lg font-semibold pb-4">
+                            Browse apps by name - {alphabet?.toUpperCase() || '-'} - {appDetails?.name}
+                        </h1>
                         <a
                             target="_blank"
                             href={`/integrations/${appDetails?.appslugname}`}
@@ -99,36 +115,78 @@ const SelectedAlphabetPage = ({ apps, alphabet, appDetails, step, alphabet2 }) =
                             <span className="text-3xl font-semibold">{appDetails?.name}</span>
                         </a>
                         <span className="text-4xl"> + </span>
-                        <h1 className="text-center lg:text-2xl md:text-xl text-lg font-semibold pb-4">
-                            Browse apps by name - {alphabet?.toUpperCase() || '-'} - {appDetails?.name} -{' '}
-                            {alphabet2?.toUpperCase()}
-                        </h1>
-                        <AlphabeticalComponent alphabet={alphabet} appDetails={appDetails} step={step} />
+                        <AlphabetComp alphabet={alphabet} appDetails={appDetails} step={step} />
                     </div>
+                </>
+            )}
 
-                    <div className=" w-full grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 justify-center items-center py-4">
-                        {apps?.map((app) => {
-                            if (app?.appslugname !== appDetails?.appslugname) {
-                                return (
-                                    <a
-                                        key={app?.rowid}
-                                        href={
-                                            app?.appslugname
-                                                ? `/integrations/${appDetails?.appslugname}/${app?.appslugname}`
-                                                : `/noplugin`
-                                        }
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="block"
-                                        aria-label="app"
-                                    >
-                                        <p className="hover:text-blue-600 text-base py-1 ">{app.name}</p>
-                                    </a>
-                                );
-                            }
-                        })}
+            {step == 3 && (
+                <>
+                    <Head>
+                        <title>
+                            Integrate {appDetails?.name} with Apps Starting with '{alphabet2?.toUpperCase()}'
+                        </title>
+                        <meta
+                            name="description"
+                            content={`Connect ${appDetails?.name} with apps beginning with '${alphabet2?.toUpperCase()}' and enhance your workflow. Discover seamless integrations to boost your productivity.`}
+                        />
+                        <meta
+                            property="og:title"
+                            content={`  Integrate ${appDetails?.name} with Apps Starting with '${alphabet2?.toUpperCase()}'`}
+                        />
+                        <meta
+                            property="og:description"
+                            content={`Connect ${appDetails?.name} with apps beginning with '${alphabet2?.toUpperCase()}' and enhance your workflow. Discover seamless integrations to boost your productivity.`}
+                        />
+                    </Head>
+                    <div className=" w-full flex flex-col gap-8 items-center">
+                        <div className="flex flex-col gap-6 items-center">
+                            <a
+                                target="_blank"
+                                href={`/integrations/${appDetails?.appslugname}`}
+                                className="flex gap-2  items-center"
+                            >
+                                <Image
+                                    src={appDetails?.iconurl}
+                                    className="h-8 w-fit"
+                                    width={46}
+                                    height={46}
+                                    alt={appDetails?.name}
+                                />
+                                <span className="text-3xl font-semibold">{appDetails?.name}</span>
+                            </a>
+                            <span className="text-4xl"> + </span>
+                            <h1 className="text-center lg:text-2xl md:text-xl text-lg font-semibold pb-4">
+                                Browse apps by name - {alphabet?.toUpperCase() || '-'} - {appDetails?.name} -{' '}
+                                {alphabet2?.toUpperCase()}
+                            </h1>
+                            <AlphabetComp alphabet={alphabet} appDetails={appDetails} step={step} />
+                        </div>
+
+                        <div className=" w-full grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 justify-center items-center py-4">
+                            {apps?.map((app) => {
+                                if (app?.appslugname !== appDetails?.appslugname) {
+                                    return (
+                                        <a
+                                            key={app?.rowid}
+                                            href={
+                                                app?.appslugname
+                                                    ? `/integrations/${appDetails?.appslugname}/${app?.appslugname}`
+                                                    : `/noplugin`
+                                            }
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="block"
+                                            aria-label="app"
+                                        >
+                                            <p className="hover:text-blue-600 text-base py-1 ">{app.name}</p>
+                                        </a>
+                                    );
+                                }
+                            })}
+                        </div>
                     </div>
-                </div>
+                </>
             )}
         </div>
     );
